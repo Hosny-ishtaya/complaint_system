f9cdbe6267a99b24746db848877a39b05018db0a
hosny-ishtaya <hosnyish812@gmail.com>
2022-11-07 14:36:49 +0200

    bulid footer and edit some css

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,12 +2,41 @@ import React from "react";
 
 
 
+
 const Footer=()=>{
     return(
 
-<div className="footer">
-  <p>footer</p>
-</div>
+      <footer>
+
+        <div className="waves">
+          <div className="wave" id="wave1"></div>
+          <div className="wave" id="wave2"></div>
+          <div className="wave" id="wave3"></div>
+          <div className="wave" id="wave4"></div>
+        </div>
+
+
+        <ul className="social-icon">
+
+          <li><a href="#"><ion-icon name="logo-facebook"></ion-icon></a></li>
+          <li><a href="#"><ion-icon name="logo-instagram"></ion-icon></a></li>
+          <li><a href="#"><ion-icon name="logo-twitter"></ion-icon></a></li>
+          <li><a href="#"><ion-icon name="logo-whatsapp"></ion-icon></a></li>
+        </ul>
+
+        <ul className="bottom-menu" >
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About us</a></li>
+            <li><a href="/service">Service</a></li>
+            <li><a href="/contact">Contact Us</a></li>       
+        </ul>
+        <p>@2022 Eng Hosny ishtaya | All Right Reserved</p>
+        
+
+
+      </footer>
+
+
 
 
     )
